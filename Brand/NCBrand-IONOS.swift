e54953d2f84997f815ec11f40adfe8038c321351
Vitalii Tolkach <v.tolkach@viseven.com>
2025-05-13 12:29:04 +0200

    make Alpha build config in order to maintain NextCloud files unchanged

--- Brand/NCBrand-IONOS.swift
+++ Brand/NCBrand-IONOS.swift
@@ -80,76 +80,78 @@ class NCBrandOptionsIONOS: NCBrandOptions, @unchecked Sendable {
 	}
 	
 	//MARK: -
-    override init() {
-        super.init()
-        disable_intro = true
-        disable_request_login_url = true
-        disable_crash_service = true
+	override init() {
+		super.init()
+		disable_intro = true
+		disable_request_login_url = true
+		disable_crash_service = true
 
-#if BETA
-        capabilitiesGroup = "group.de.strato.ionos.easystorage.beta"
+#if ALPHA
+		capabilitiesGroup = "group.com.viseven.ionos.easystorage"
+#elseif BETA
+		capabilitiesGroup = "group.de.strato.ionos.easystorage.beta"
 #elseif APPSTORE
-        capabilitiesGroup = "group.com.ionos.hidrivenext"
+		capabilitiesGroup = "group.com.ionos.hidrivenext"
 #else
-        capabilitiesGroup = "group.com.viseven.ionos.easystorage"
+		capabilitiesGroup = "group.com.viseven.ionos.easystorage"
 #endif
-    }
+	}
 }
 
 extension NCBrandOptions {
-    var acknowloedgements: String {
-        "https://wl.hidrive.com/easy/0171"
-    }
+	var acknowloedgements: String {
+		"https://wl.hidrive.com/easy/0171"
+	}
 }
 
 class NCBrandColorIONOS: NCBrandColor, @unchecked Sendable {
-    
-    static let ionosBrand = UIColor(red: 20.0 / 255.0, green: 116.0 / 255.0, blue: 196.0 / 255.0, alpha: 1.0) // BLUE IONOS : #1474C4
 	
-    override func getElement(account: String?) -> UIColor {
+	static let ionosBrand = UIColor(red: 20.0 / 255.0, green: 116.0 / 255.0, blue: 196.0 / 255.0, alpha: 1.0) // BLUE IONOS : #1474C4
+	
+	override func getElement(account: String?) -> UIColor {
 		if customer == UIColor(red: 0.0 / 255.0, green: 130.0 / 255.0, blue: 201.0 / 255.0, alpha: 1.0) { // default NC color
 			return NCBrandColorIONOS.ionosBrand
 		}
-        return customer
-    }
+		return super.getElement(account: account)
+	}
 }
 
 extension NCBrandColor {
-    var brandElement: UIColor {
-        return NCBrandColorIONOS.ionosBrand
-    }
-    
+	var brandElement: UIColor {
+		return customer
+	}
+	
 #if !EXTENSION || EXTENSION_SHARE
-    var menuIconColor: UIColor {
-        UIColor(resource: .FileMenu.icon)
-    }
-    
-    var menuFolderIconColor: UIColor {
-        UIColor(resource: .FileMenu.folderIcon)
-    }
-    
-    var appBackgroundColor: UIColor {
-        UIColor(resource: .AppBackground.main)
-    }
-    
-    var formBackgroundColor: UIColor {
-        UIColor(resource: .AppBackground.form)
-    }
-    
-    var formRowBackgroundColor: UIColor {
-        UIColor(resource: .AppBackground.formRow)
-    }
-    
-    var formSeparatorColor: UIColor {
-        UIColor(resource: .formSeparator)
-    }
+	var menuIconColor: UIColor {
+		UIColor(resource: .FileMenu.icon)
+	}
+	
+	var menuFolderIconColor: UIColor {
+		UIColor(resource: .FileMenu.folderIcon)
+	}
+	
+	var appBackgroundColor: UIColor {
+		UIColor(resource: .AppBackground.main)
+	}
+	
+	var formBackgroundColor: UIColor {
+		UIColor(resource: .AppBackground.form)
+	}
+	
+	var formRowBackgroundColor: UIColor {
+		UIColor(resource: .AppBackground.formRow)
+	}
+	
+	var formSeparatorColor: UIColor {
+		UIColor(resource: .formSeparator)
+	}
 #endif
-    
-    var switchColor: UIColor {
-        return UIColor { traits in
-            let light = self.brandElement
-            let dark = UIColor(red: 17.0 / 255.0, green: 199.0 / 255.0, blue: 230.0 / 255.0, alpha: 1.0)
-            return traits.userInterfaceStyle == .dark ? dark : light
-        }
-    }
+	
+	var switchColor: UIColor {
+		return UIColor { traits in
+			let light = self.brandElement
+			let dark = UIColor(red: 17.0 / 255.0, green: 199.0 / 255.0, blue: 230.0 / 255.0, alpha: 1.0)
+			return traits.userInterfaceStyle == .dark ? dark : light
+		}
+	}
 }
