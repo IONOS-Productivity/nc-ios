bee523f38c39f6ddbfa75955c683f3bd6e4e834f
Vitalii Tolkach <v.tolkach@viseven.com>
2025-05-16 10:31:01 +0200

    TELKOMAUST-860: info label reverted

--- iOSClient/Main/Collection Common/NCCollectionViewCommon+CollectionViewDataSource.swift
+++ iOSClient/Main/Collection Common/NCCollectionViewCommon+CollectionViewDataSource.swift
@@ -365,7 +365,6 @@ extension NCCollectionViewCommon: UICollectionViewDataSource {
 
 		// Status
 		cell.fileStatusImage?.image = nil
-		cell.fileInfoLabel?.text = nil
 
 		if metadata.isLivePhoto {
 			cell.fileStatusImage?.image = utility.loadImage(named: "livephoto", colors: isLayoutPhoto ? [.white] : [NCBrandColor.shared.iconImageColor2])
